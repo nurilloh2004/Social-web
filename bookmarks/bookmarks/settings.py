67c80addfd0f627562cab75984a89f9aec2a01b3
nurilloh.2004 <nurillohpython@gmail.com>
2022-07-26 21:42:54 +0500

    settibgs file

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#26bs$$g@g(#0j88$%30b3waj_v4vc5j9$oi_1%v+__)+n5^!n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['nurilloh_an.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
             ],
         },
     },
@@ -74,6 +75,28 @@ TEMPLATES = [
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 
 
+WSGI_APPLICATION = 'bookmarks.wsgi.application'
+
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
+    'social_core.backends.google.GoogleOpenId',  # for Google authentication
+    'social_core.backends.google.GoogleOAuth2',  # for Google authentication
+    'social_core.backends.linkedin.LinkedinOAuth2',  # for Github authentication
+    'social_core.backends.facebook.FacebookOAuth2',  # for Facebook authentication
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+
+
+SOCIAL_AUTH_FACEBOOK_KEY = '' # Facebook App ID
+SOCIAL_AUTH_FACEBOOK_SECRET = '' # Facebook App Secret
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+
+SOCIAL_AUTH_TWITTER_KEY = '' # Twitter API Key
+SOCIAL_AUTH_TWITTER_SECRET = '' # Twitter API Secret
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '' # Google Consumer Key
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '' # Google Consumer Secret
 
 WSGI_APPLICATION = 'bookmarks.wsgi.application'
 
