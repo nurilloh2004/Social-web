63e53eaff83e7076bae591e5f7fc000ee3ce9371
nurilloh2004 <nurillohpython@gmail.com>
2022-07-27 15:14:49 -0400

    model file

--- bookmarks/bookmarks/urls.py
+++ bookmarks/bookmarks/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('social-auth/', include('social_django.urls', namespace='social')),
-    # path()
+    path('images/', include('images.urls', namespace='images')),
+    
 ]
 
 
